712395345f0ca46a76cacbda5cb0042661f4e2bb
markofrontend <markovucurovic@gmail.com>
2022-06-25 11:37:43 +0200

    Add new arrivals promo

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,14 +1,17 @@
 <template>
-  <div>
-    <h1>
-      Home
-    </h1>
-  </div>
+  <main>
+    <NewArrivalsPromo></NewArrivalsPromo>
+  </main>
 </template>
 
 <script>
+import NewArrivalsPromo from "@/components/promo/NewArrivalsPromo";
 
 export default {
   name: 'HomeView',
+
+  components: {
+    NewArrivalsPromo,
+  }
 }
 </script>
