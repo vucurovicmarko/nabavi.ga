3c33d00abe78bbb79491c7229699e36c2e6268a3
markofrontend <markovucurovic@gmail.com>
2022-06-25 18:57:25 +0200

    Router scroll behavior

--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -1,22 +1,31 @@
 <template>
-  <div class="group relative">
-    <div
-        class="w-full min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:h-80 lg:aspect-none">
-      <img src="https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg"
-           alt="Front of men&#039;s Basic Tee in black."
-           class="w-full h-full object-center object-cover lg:w-full lg:h-full">
-    </div>
-    <div class="mt-4 flex justify-between">
-      <div>
-        <h3 class="text-sm text-gray-700">
-          <router-link :to="{ name: 'product', params: { product: '1' }}">
-            <span aria-hidden="true" class="absolute inset-0"></span>
-            Basic Tee
-          </router-link>
-        </h3>
+  <div>
+    <div class="group relative">
+      <div
+          class="w-full min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:h-80 lg:aspect-none">
+        <img src="https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg"
+             alt="Front of men&#039;s Basic Tee in black."
+             class="w-full h-full object-center object-cover lg:w-full lg:h-full">
       </div>
-      <p class="text-sm font-medium text-gray-900">$35</p>
+      <div class="mt-4 flex justify-between">
+        <div>
+          <h3 class="text-sm text-gray-700">
+            <router-link :to="{ name: 'product', params: { product: '1' }}">
+              <span aria-hidden="true" class="absolute inset-0"></span>
+              Basic Tee
+            </router-link>
+          </h3>
+        </div>
+        <p class="text-sm font-medium text-gray-900">$35</p>
+      </div>
     </div>
+
+    <div class="mt-5">
+      <button type="button" class="w-full px-4 py-2 border border-transparent text-sm font-medium rounded-md text-gray-700 bg-gray-100 hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500">
+        Add to bag
+      </button>
+    </div>
+
   </div>
 </template>
 
--- src/router/index.js
+++ src/router/index.js
@@ -34,6 +34,9 @@ const routes = [
 ];
 
 const router = createRouter({
+    scrollBehavior(to, from, savedPosition) {
+        return { top: 0 }
+    },
     history: createWebHistory(process.env.BASE_URL),
     routes
 });
