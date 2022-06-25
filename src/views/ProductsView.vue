8ec74a4cfaf0ab9f73c503b7cc27e0aa54237867
markofrontend <markovucurovic@gmail.com>
2022-06-25 15:01:42 +0200

    Mobile filters expand, sort options

--- src/views/ProductsView.vue
+++ src/views/ProductsView.vue
@@ -45,72 +45,69 @@
                 <!-- Filters -->
                 <form class="mt-4 border-t border-gray-200">
                   <div class="px-4 py-6">
-                    <h3 class="-mx-2 -my-3 flow-root">
-                      <!-- Expand/collapse section button -->
-                      <button type="button"
-                              class="px-2 py-3 bg-white w-full flex items-center justify-between text-gray-400 hover:text-gray-500">
-                        <span class="font-medium text-gray-900"> Category </span>
-                        <span class="ml-6 flex items-center">
-                    <!--
-                      Expand icon, show/hide based on section open state.
+                    <VExpand>
+                      <template #trigger="{open}">
+                        <h3 class="-mx-2 -my-3 flow-root">
+                          <button type="button"
+                                  class="px-2 py-3 bg-white w-full flex items-center justify-between text-gray-400 hover:text-gray-500">
+                            <span class="font-medium text-gray-900"> Category </span>
+                            <div class="ml-6 flex items-center">
+                              <svg v-if="!open" class="h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"
+                                   fill="currentColor">
+                                <path fill-rule="evenodd"
+                                      d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z"
+                                      clip-rule="evenodd"/>
+                              </svg>
+                              <svg v-else class="h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"
+                                   fill="currentColor">
+                                <path fill-rule="evenodd" d="M5 10a1 1 0 011-1h8a1 1 0 110 2H6a1 1 0 01-1-1z"
+                                      clip-rule="evenodd"/>
+                              </svg>
+                            </div>
+                          </button>
+                        </h3>
+                      </template>
 
-                      Heroicon name: solid/plus-sm
-                    -->
-                    <svg class="h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
-                      <path fill-rule="evenodd"
-                            d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z"
-                            clip-rule="evenodd"/>
-                    </svg>
-                          <!--
-                            Collapse icon, show/hide based on section open state.
+                      <div class="pt-6">
+                        <div class="space-y-6">
+                          <div class="flex items-center">
+                            <input id="filter-mobile-category-0" name="category[]" value="new-arrivals" type="checkbox"
+                                   class="h-4 w-4 border-gray-300 rounded text-emerald-600 focus:ring-emerald-500">
+                            <label for="filter-mobile-category-0" class="ml-3 min-w-0 flex-1 text-gray-500"> New
+                              Arrivals </label>
+                          </div>
 
-                            Heroicon name: solid/minus-sm
-                          -->
-                    <svg class="h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
-                      <path fill-rule="evenodd" d="M5 10a1 1 0 011-1h8a1 1 0 110 2H6a1 1 0 01-1-1z"
-                            clip-rule="evenodd"/>
-                    </svg>
-                  </span>
-                      </button>
-                    </h3>
-                    <!-- Filter section, show/hide based on section state. -->
-                    <div class="pt-6" id="filter-section-mobile-1">
-                      <div class="space-y-6">
-                        <div class="flex items-center">
-                          <input id="filter-mobile-category-0" name="category[]" value="new-arrivals" type="checkbox"
-                                 class="h-4 w-4 border-gray-300 rounded text-emerald-600 focus:ring-emerald-500">
-                          <label for="filter-mobile-category-0" class="ml-3 min-w-0 flex-1 text-gray-500"> New
-                            Arrivals </label>
-                        </div>
+                          <div class="flex items-center">
+                            <input id="filter-mobile-category-1" name="category[]" value="sale" type="checkbox"
+                                   class="h-4 w-4 border-gray-300 rounded text-emerald-600 focus:ring-emerald-500">
+                            <label for="filter-mobile-category-1" class="ml-3 min-w-0 flex-1 text-gray-500">
+                              Sale </label>
+                          </div>
 
-                        <div class="flex items-center">
-                          <input id="filter-mobile-category-1" name="category[]" value="sale" type="checkbox"
-                                 class="h-4 w-4 border-gray-300 rounded text-emerald-600 focus:ring-emerald-500">
-                          <label for="filter-mobile-category-1" class="ml-3 min-w-0 flex-1 text-gray-500"> Sale </label>
-                        </div>
+                          <div class="flex items-center">
+                            <input id="filter-mobile-category-2" name="category[]" value="travel" type="checkbox"
+                                   checked
+                                   class="h-4 w-4 border-gray-300 rounded text-emerald-600 focus:ring-emerald-500">
+                            <label for="filter-mobile-category-2" class="ml-3 min-w-0 flex-1 text-gray-500">
+                              Travel </label>
+                          </div>
 
-                        <div class="flex items-center">
-                          <input id="filter-mobile-category-2" name="category[]" value="travel" type="checkbox" checked
-                                 class="h-4 w-4 border-gray-300 rounded text-emerald-600 focus:ring-emerald-500">
-                          <label for="filter-mobile-category-2" class="ml-3 min-w-0 flex-1 text-gray-500">
-                            Travel </label>
-                        </div>
+                          <div class="flex items-center">
+                            <input id="filter-mobile-category-3" name="category[]" value="organization" type="checkbox"
+                                   class="h-4 w-4 border-gray-300 rounded text-emerald-600 focus:ring-emerald-500">
+                            <label for="filter-mobile-category-3" class="ml-3 min-w-0 flex-1 text-gray-500">
+                              Organization </label>
+                          </div>
 
-                        <div class="flex items-center">
-                          <input id="filter-mobile-category-3" name="category[]" value="organization" type="checkbox"
-                                 class="h-4 w-4 border-gray-300 rounded text-emerald-600 focus:ring-emerald-500">
-                          <label for="filter-mobile-category-3" class="ml-3 min-w-0 flex-1 text-gray-500">
-                            Organization </label>
-                        </div>
-
-                        <div class="flex items-center">
-                          <input id="filter-mobile-category-4" name="category[]" value="accessories" type="checkbox"
-                                 class="h-4 w-4 border-gray-300 rounded text-emerald-600 focus:ring-emerald-500">
-                          <label for="filter-mobile-category-4" class="ml-3 min-w-0 flex-1 text-gray-500">
-                            Accessories </label>
+                          <div class="flex items-center">
+                            <input id="filter-mobile-category-4" name="category[]" value="accessories" type="checkbox"
+                                   class="h-4 w-4 border-gray-300 rounded text-emerald-600 focus:ring-emerald-500">
+                            <label for="filter-mobile-category-4" class="ml-3 min-w-0 flex-1 text-gray-500">
+                              Accessories </label>
+                          </div>
                         </div>
                       </div>
-                    </div>
+                    </VExpand>
                   </div>
                 </form>
               </div>
@@ -142,19 +139,14 @@
               </template>
 
               <div class="py-1">
-                <!--
-                  Active: "bg-gray-100", Not Active: ""
-                  Selected: "font-medium text-gray-900", Not Selected: "text-gray-500"
-                -->
-                <a href="#" class="font-medium text-gray-900 block px-4 py-2 text-sm" tabindex="-1"> Most Popular </a>
-
-                <a href="#" class="text-gray-500 block px-4 py-2 text-sm" tabindex="-1"> Best Rating </a>
-
-                <a href="#" class="text-gray-500 block px-4 py-2 text-sm" tabindex="-1"> Newest </a>
-
-                <a href="#" class="text-gray-500 block px-4 py-2 text-sm" tabindex="-1"> Price: Low to High </a>
-
-                <a href="#" class="text-gray-500 block px-4 py-2 text-sm" tabindex="-1"> Price: High to Low </a>
+                <button v-for="option in sortOptions"
+                        class="block w-full px-4 py-2 text-left text-sm hover:bg-gray-100"
+                        :class="filters.sort === option ? 'font-medium text-gray-900' : 'text-gray-500'"
+                        @click="handleSortOptionClick(option)"
+                        :key="option"
+                >
+                  {{ option }}
+                </button>
               </div>
             </VDropdown>
 
@@ -271,12 +263,18 @@ export default {
   data() {
     return {
       filters: {
+        sort: 'Newest',
         categories: [],
       },
+      sortOptions: ['Newest', 'Price: Low to High', 'Price: High to Low'],
       mobileFiltersOpen: false,
     }
   },
   methods: {
+    handleSortOptionClick(option) {
+      this.filters.sort = option;
+      this.$refs['sortDropdown'].close();
+    },
     openMobileFilters() {
       this.mobileFiltersOpen = true;
     },
