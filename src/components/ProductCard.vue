dbfa19dbe3b139c43b41f6c45d75a9fce2cf9de2
markofrontend <markovucurovic@gmail.com>
2022-06-26 14:58:25 +0200

    Set default product img on error

--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -4,7 +4,9 @@
       <div class="w-full min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:h-80 lg:aspect-none">
         <img :src="product.get_thumbnail"
              :alt="product.name"
-             class="w-full h-full object-center object-cover lg:w-full lg:h-full">
+             class="w-full h-full object-center object-cover lg:w-full lg:h-full"
+             @error="setDefaultImg"
+        >
       </div>
       <div class="mt-4 flex justify-between">
         <div>
@@ -37,6 +39,11 @@ export default {
       type: Object,
       required: true,
     },
+  },
+  methods: {
+    setDefaultImg(e) {
+      e.target.src = require("@/assets/images/default-product.jpg");
+    }
   }
 }
 </script>
