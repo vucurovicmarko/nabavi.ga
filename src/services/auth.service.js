972583063099f110e12d7e63489865cfad0d88f8
markofrontend <markovucurovic@gmail.com>
2022-07-02 19:37:08 +0200

    AuthService getUser & login methods

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,8 +1,16 @@
 import axios from "axios";
 
 class AuthService {
+  getUser() {
+    return axios.get("users/me/");
+  }
+
   register(data) {
     return axios.post("users/", data);
+  }
+
+  login(data) {
+    return axios.post("jwt/create/", data);
   }
 }
 
