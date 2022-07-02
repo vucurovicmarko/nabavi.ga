04cf632830672a09db6f8ee3913550577bba48b0
markofrontend <markovucurovic@gmail.com>
2022-07-02 08:37:24 +0200

    App layout

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,43 @@
 <template>
-  <TheNavbar v-if="!authLayout"></TheNavbar>
-
-  <div class="flex-grow flex flex-col">
-    <RouterView />
-  </div>
-
-  <TheFooter v-if="!authLayout"></TheFooter>
+  <AppLayout v-if="!authLayout"></AppLayout>
+  <RouterView v-else />
 </template>
 
 <script>
-import { mapActions } from "pinia";
+import axios from "axios";
+
+import { mapState, mapActions } from "pinia";
 import { useCategoryStore } from "@/stores/category";
+import { useAuthStore } from "./stores/auth";
 import { useCartStore } from "@/stores/cart";
 
-import TheNavbar from "@/views/partials/TheNavbar";
-import TheFooter from "@/views/partials/TheFooter";
+import AppLayout from "@/views/layouts/AppLayout";
 
 export default {
   components: {
-    TheNavbar,
-    TheFooter,
+    AppLayout,
   },
   computed: {
+    ...mapState[(useAuthStore, ["accessToken"])],
     authLayout() {
-      return this.$route.meta.layout === 'auth';
-    }
+      return this.$route.meta.layout === "auth";
+    },
   },
   created() {
+    this.initializeAuth();
     this.fetchCategories();
     this.initializeCart();
+
+    if (this.accessToken) {
+      axios.defaults.headers.common[
+        "Authorization"
+      ] = `JWT ${this.accessToken}`;
+    }
   },
   methods: {
     ...mapActions(useCategoryStore, ["fetchCategories"]),
     ...mapActions(useCartStore, ["initializeCart"]),
+    ...mapActions(useAuthStore, ["initializeAuth"]),
   },
 };
 </script>
---
+++ src/views/layouts/AppLayout.vue
@@ -0,0 +1,24 @@
+<template>
+  <TheNavbar></TheNavbar>
+
+  <div class="flex-grow flex flex-col">
+    <RouterView />
+  </div>
+
+  <TheFooter></TheFooter>
+</template>
+
+<script>
+import TheNavbar from "@/views/partials/TheNavbar";
+import TheFooter from "@/views/partials/TheFooter";
+
+export default {
+  name: "AppLayout",
+  components: {
+    TheNavbar,
+    TheFooter,
+  },
+};
+</script>
+
+<style scoped></style>
