4e675f7559088a628a18fe3bb704df84b65934ca
markofrontend <markovucurovic@gmail.com>
2022-07-02 19:38:49 +0200

    Auth store jwt tokens & user handling

--- src/stores/auth.js
+++ src/stores/auth.js
@@ -1,22 +1,81 @@
+import axios from "axios";
+
 import { defineStore } from "pinia";
 
-import AuthService from "@/services/auth.service";
+const LOCAL_STORAGE_ACCESS_TOKEN_KEY = "accessToken";
+const LOCAL_STORAGE_REFRESH_TOKEN_KEY = "refreshToken";
 
 export const useAuthStore = defineStore("auth", {
   state: () => ({
+    user: {},
     accessToken: "",
     refreshToken: "",
   }),
+  getters: {
+    hasAccessToken() {
+      return Boolean(this.accessToken);
+    },
+  },
   actions: {
     initializeAuth() {
-      const accessToken = localStorage.getItem("accessToken");
+      this.accessToken = this.getLocalStorageAccessToken() || "";
+      this.refreshToken = this.getLocalStorageRefreshToken() || "";
 
-      if (accessToken) {
-        this.accessToken = accessToken;
-      }
+      this.setAuthHeader();
+    },
+    setAuth(accessToken, refreshToken) {
+      this.setAccessToken(accessToken);
+      this.setRefreshToken(refreshToken);
+
+      this.setLocalStorageAccessToken(accessToken);
+      this.setLocalStorageRefreshToken(refreshToken);
+
+      this.setAuthHeader();
+    },
+    purgeAuth() {
+      this.accessToken = "";
+      this.refreshToken = "";
+      this.user = {};
+
+      this.removeLocalStorageAccessToken();
+      this.removeLocalStorageRefreshToken();
+
+      this.setAuthHeader();
+    },
+    setUser(user) {
+      this.user = user;
     },
     setAccessToken(accessToken) {
       this.accessToken = accessToken;
+    },
+    setRefreshToken(refreshToken) {
+      this.refreshToken = refreshToken;
+    },
+    setAuthHeader() {
+      axios.defaults.headers.common["Authorization"] = this.accessToken
+        ? `JWT ${this.accessToken}`
+        : "";
+    },
+    getLocalStorageAccessToken() {
+      return localStorage.getItem(LOCAL_STORAGE_ACCESS_TOKEN_KEY);
+    },
+    getLocalStorageRefreshToken() {
+      return localStorage.getItem(LOCAL_STORAGE_REFRESH_TOKEN_KEY);
+    },
+    setLocalStorageAccessToken(accessToken) {
+      return localStorage.setItem(LOCAL_STORAGE_ACCESS_TOKEN_KEY, accessToken);
+    },
+    setLocalStorageRefreshToken(refreshToken) {
+      return localStorage.setItem(
+        LOCAL_STORAGE_REFRESH_TOKEN_KEY,
+        refreshToken
+      );
+    },
+    removeLocalStorageAccessToken() {
+      return localStorage.removeItem(LOCAL_STORAGE_ACCESS_TOKEN_KEY);
+    },
+    removeLocalStorageRefreshToken() {
+      return localStorage.removeItem(LOCAL_STORAGE_REFRESH_TOKEN_KEY);
     },
   },
 });
