fd132f694e07316b2bad3474257064f9cbfce45b
markofrontend <markovucurovic@gmail.com>
2022-07-02 19:42:39 +0200

    Init auth, move cart & categories init

--- src/App.vue
+++ src/App.vue
@@ -4,12 +4,8 @@
 </template>
 
 <script>
-import axios from "axios";
-
-import { mapState, mapActions } from "pinia";
-import { useCategoryStore } from "@/stores/category";
-import { useAuthStore } from "./stores/auth";
-import { useCartStore } from "@/stores/cart";
+import { mapActions } from "pinia";
+import { useAuthStore } from "@/stores/auth";
 
 import AppLayout from "@/views/layouts/AppLayout";
 
@@ -18,25 +14,14 @@ export default {
     AppLayout,
   },
   computed: {
-    ...mapState[(useAuthStore, ["accessToken"])],
     authLayout() {
       return this.$route.meta.layout === "auth";
     },
   },
   created() {
     this.initializeAuth();
-    this.fetchCategories();
-    this.initializeCart();
-
-    if (this.accessToken) {
-      axios.defaults.headers.common[
-        "Authorization"
-      ] = `JWT ${this.accessToken}`;
-    }
   },
   methods: {
-    ...mapActions(useCategoryStore, ["fetchCategories"]),
-    ...mapActions(useCartStore, ["initializeCart"]),
     ...mapActions(useAuthStore, ["initializeAuth"]),
   },
 };
--- src/views/layouts/AppLayout.vue
+++ src/views/layouts/AppLayout.vue
@@ -9,6 +9,14 @@
 </template>
 
 <script>
+import { mapState, mapActions } from "pinia";
+
+import { useAuthStore } from "@/stores/auth";
+import { useCategoryStore } from "@/stores/category";
+import { useCartStore } from "@/stores/cart";
+
+import AuthService from "@/services/auth.service";
+
 import TheNavbar from "@/views/partials/TheNavbar";
 import TheFooter from "@/views/partials/TheFooter";
 
@@ -17,6 +25,28 @@ export default {
   components: {
     TheNavbar,
     TheFooter,
+  },
+  created() {
+    if (this.hasAccessToken) {
+      this.getUser();
+    }
+
+    this.initializeCart();
+    this.fetchCategories();
+  },
+  computed: {
+    ...mapState(useAuthStore, ["hasAccessToken"]),
+  },
+  methods: {
+    ...mapActions(useAuthStore, ["setUser"]),
+    ...mapActions(useCategoryStore, ["fetchCategories"]),
+    ...mapActions(useCartStore, ["initializeCart"]),
+    getUser() {
+      AuthService.getUser().then(
+        ({ data }) => this.setUser(data),
+        (error) => console.log(error)
+      );
+    },
   },
 };
 </script>
