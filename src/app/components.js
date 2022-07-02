e213461c228a64b1f5cdf4af70d97a404777c16d
markofrontend <markovucurovic@gmail.com>
2022-07-02 13:10:17 +0200

    Register button component

--- src/app/components.js
+++ src/app/components.js
@@ -2,9 +2,11 @@ import app from "@/app";
 
 import VDropdown from "@/components/VDropdown";
 import VLoader from "@/components/VLoader";
+import VButton from "@/components/VButton";
 import VLogo from "@/components/VLogo";
 
 app
-    .component("VDropdown", VDropdown)
-    .component("VLoader", VLoader)
-    .component("VLogo", VLogo)
+  .component("VDropdown", VDropdown)
+  .component("VLoader", VLoader)
+  .component("VButton", VButton)
+  .component("VLogo", VLogo);
