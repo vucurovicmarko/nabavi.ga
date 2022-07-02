5fda34678b7267870869b9c2a259f91575a68a4f
markofrontend <markovucurovic@gmail.com>
2022-07-02 19:39:51 +0200

    Format cart store

--- src/stores/cart.js
+++ src/stores/cart.js
@@ -1,73 +1,79 @@
-import {defineStore} from 'pinia';
-import {useToast} from "vue-toastification";
+import { defineStore } from "pinia";
+import { useToast, POSITION } from "vue-toastification";
 
-const LOCAL_STORAGE_CART_KEY = 'cart';
+const LOCAL_STORAGE_CART_KEY = "cart";
+
 const toast = useToast();
 
 export const useCartStore = defineStore("cart", {
-    state: () => ({
-        products: [],
-    }),
-    getters: {
-        hasProducts: (state) => state.products.length > 0,
-        productsCount: (state) => state.products.length,
-        subtotal() {
-            return this.products.reduce((memo, {price, quantity}) => {
-                return memo + price * quantity;
-            }, 0);
-        },
+  state: () => ({
+    products: [],
+  }),
+  getters: {
+    hasProducts: (state) => state.products.length > 0,
+    productsCount: (state) => state.products.length,
+    subtotal() {
+      return this.products.reduce((memo, { price, quantity }) => {
+        return memo + price * quantity;
+      }, 0);
     },
-    actions: {
-        initializeCart() {
-            const products = localStorage.getItem(LOCAL_STORAGE_CART_KEY);
+  },
+  actions: {
+    initializeCart() {
+      const products = localStorage.getItem(LOCAL_STORAGE_CART_KEY);
 
-            if (products) {
-                this.products = JSON.parse(products);
-            }
-        },
-        increaseQuantity(product) {
-            const cartProduct = this.getCartProduct(product.id);
-            cartProduct.quantity++;
-
-            this.saveCartToLocalStorage();
-        },
-        decreaseQuantity(product) {
-            const cartProduct = this.getCartProduct(product.id);
-
-            if (cartProduct.quantity > 1) {
-                cartProduct.quantity--;
-            } else {
-                this.removeProduct(product.id);
-            }
-
-            this.saveCartToLocalStorage();
-        },
-        addProduct(product) {
-            const cartProduct = this.getCartProduct(product.id);
-
-            if (cartProduct) {
-                cartProduct.quantity++;
-            } else {
-                this.products.push({...product, quantity: 1});
-            }
-
-            toast.success("Added to cart", {position: "bottom-left"});
-            this.saveCartToLocalStorage();
-        },
-        removeProduct(productId) {
-            const cartProductIndex = this.products.findIndex(({id}) => id === productId);
-
-            if (cartProductIndex !== -1) {
-                this.products.splice(cartProductIndex, 1);
-            }
-
-            this.saveCartToLocalStorage();
-        },
-        getCartProduct(productId) {
-            return this.products.find(({id}) => id === productId)
-        },
-        saveCartToLocalStorage() {
-            localStorage.setItem(LOCAL_STORAGE_CART_KEY, JSON.stringify(this.products));
-        },
+      if (products) {
+        this.products = JSON.parse(products);
+      }
     },
+    increaseQuantity(product) {
+      const cartProduct = this.getCartProduct(product.id);
+      cartProduct.quantity++;
+
+      this.saveCartToLocalStorage();
+    },
+    decreaseQuantity(product) {
+      const cartProduct = this.getCartProduct(product.id);
+
+      if (cartProduct.quantity > 1) {
+        cartProduct.quantity--;
+      } else {
+        this.removeProduct(product.id);
+      }
+
+      this.saveCartToLocalStorage();
+    },
+    addProduct(product) {
+      const cartProduct = this.getCartProduct(product.id);
+
+      if (cartProduct) {
+        cartProduct.quantity++;
+      } else {
+        this.products.push({ ...product, quantity: 1 });
+      }
+
+      toast.success("Added to cart", { position: POSITION.BOTTOM_LEFT });
+      this.saveCartToLocalStorage();
+    },
+    removeProduct(productId) {
+      const cartProductIndex = this.products.findIndex(
+        ({ id }) => id === productId
+      );
+
+      if (cartProductIndex !== -1) {
+        this.products.splice(cartProductIndex, 1);
+      }
+
+      this.saveCartToLocalStorage();
+    },
+    getCartProduct(productId) {
+      return this.products.find(({ id }) => id === productId);
+    },
+    saveCartToLocalStorage() {
+      localStorage.setItem(
+        LOCAL_STORAGE_CART_KEY,
+        JSON.stringify(this.products)
+      );
+    },
+  },
 });
