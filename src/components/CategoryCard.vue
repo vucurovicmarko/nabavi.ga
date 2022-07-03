a09c4aa35213768308e337c3c07c85b327948f12
markofrontend <markovucurovic@gmail.com>
2022-07-03 23:21:34 +0200

    Move static category imgs to public, handle default img

--- src/components/CategoryCard.vue
+++ src/components/CategoryCard.vue
@@ -1,14 +1,19 @@
 <template>
   <div class="group relative">
-    <div class="relative w-full h-80 bg-white rounded-lg overflow-hidden group-hover:opacity-75 sm:aspect-w-2 sm:aspect-h-1 sm:h-64 lg:aspect-w-1 lg:aspect-h-1">
-      <img :src="require(`@/assets/images/categories/${category.slug}.jpg`)"
-           :alt="category.name"
-           class="w-full h-full object-center object-cover"
-      >
+    <div
+      class="relative w-full h-80 bg-white rounded-lg overflow-hidden group-hover:opacity-75 sm:aspect-w-2 sm:aspect-h-1 sm:h-64 lg:aspect-w-1 lg:aspect-h-1"
+    >
+      <img
+        :src="`/images/categories/${category.slug}.jpg`"
+        :alt="category.name"
+        class="w-full h-full object-center object-cover"
+        @error="defaultImg"
+      />
     </div>
     <h3 class="mt-6">
-      <router-link :to="category.get_absolute_url"
-                   class="text-base font-semibold text-gray-900"
+      <router-link
+        :to="category.get_absolute_url"
+        class="text-base font-semibold text-gray-900"
       >
         <span class="absolute inset-0"></span>
         {{ category.name }}
@@ -26,9 +31,12 @@ export default {
       required: true,
     },
   },
-}
+  methods: {
+    defaultImg(e) {
+      e.target.src = "/images/categories/default.jpg";
+    },
+  },
+};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
